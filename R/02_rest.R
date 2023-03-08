6ec939eff0f272e695b20a54028719d944eb550a
Cher Chow <cher.fyc@gmail.com>
2023-03-08 11:44:06 +0000

    add error handling if optimisation returns infinite values error

--- R/02_rest.R
+++ R/02_rest.R
@@ -38,6 +38,7 @@ restsp <- data_ruv %>% group_by(site_ID, Taxon) %>% summarise(occurrences = sum(
   filter(occurrences >= 10) %>% pull(Taxon) %>% sort %>% unique
 
 output_poisson <- data.frame(site_ID = '', Taxon = rep(restsp, each = 2), shapeET = '', lambdaEY = '')
+output_poisson_hess <- as.list(rep(0, length(restsp)*2))
 
 # NEST LEVELS: species-site-camera-detection
 
@@ -60,7 +61,7 @@ for (i in 1:length(restsp)) {
                                     Camera == sitecam[sitecam$site_ID == unique(data_ruv$site_ID)[s],2][n]) %>% pull(staytime)
         # do the most nested level first, Tij staying time
         for (x in 1:n) {
-          Tij[x] <- sum(dgamma(x = stay, shape = par[1], scale = 1, log = T)) # params[1] = shape
+          Tij[x] <- dgamma(x = stay, shape = par[1], scale = 1, log = T) %>% sum # params[1] = shape
         }
       }
       return(
@@ -68,15 +69,26 @@ for (i in 1:length(restsp)) {
       )
     }
     # put that through the optimise function
-    output_poisson[2*i-s+1,3:4] <- optim(par = c(1,4), REST_poisson)$par
+    # with error handling
+    optout <- tryCatch(optim(par = c(0.5,9), REST_poisson, method = "L-BFGS-B", lower = c(0,0), upper = c(2000,200), hessian = T),
+                       error = function(e) e)
+    if(inherits(optout, "error")) next # if an error message gets generated this run, move to the next iteration of the loop
+    
+    output_poisson[2*i-s+1,3:4] <- optout$par
+    output_poisson_hess[[2*i-s+1]] <- optout$hessian
+    
+    # throw in secondary field values for the data output table
     output_poisson$site_ID[2*i-s+1] <- unique(data_ruv$site_ID)[s]
+    output_poisson[[3]] <- as.numeric(output_poisson[[3]])
+    output_poisson[[4]]  <- as.numeric(output_poisson[[4]])
+    output_poisson$D <- (output_poisson$shapeET*output_poisson$lambdaEY / (2700 * 4 * cams[s]))*250 # scale to be density within belt transect
   }
 }
+
+
 
 # interpret outputs
 # gamma shape = mean = E(T)
 # poisson lambda = mean = variance = E(Y)
 
-output_poisson[[3]] <- as.numeric(output_poisson[[3]])
-output_poisson[[4]]  <- as.numeric(output_poisson[[4]])
-output_poisson$D <- (output_poisson$shapeET*output_poisson$lambdaEY / (2700 * 4))*250 # scale to be density within belt transect
+
